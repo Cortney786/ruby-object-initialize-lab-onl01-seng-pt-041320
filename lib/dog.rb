0908642a75d7a91444558141cf3c3fcb14ca566e
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-28 19:22:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   def initialize(dogs_name, dogs_breed)
     @name = dogs_name 
-    
+    @breed = dogs_breed
   end 
   
-  def name=(name)
+  def name=(dogs_name, dogs_breed)
     @name = name 
   end 
   
